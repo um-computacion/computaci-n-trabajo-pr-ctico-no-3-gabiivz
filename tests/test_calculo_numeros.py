0b67d04fdde1726e349ace54667e75f34207870c
gabiivz <g.vaca@alumno.um.edu.ar>
2025-04-15 20:52:20 -0300

    implementacion de test para numeros negativos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,5 +1,8 @@
 import unittest
-from src.exceptions import (ingrese_numero, NumeroDebeSerPositivo)
+from src.exceptions import (
+    ingrese_numero,
+    NumeroDebeSerPositivo,
+)
 from unittest.mock import patch
 
 class TestCalculoNumeros(unittest.TestCase):
@@ -12,7 +15,15 @@ class TestCalculoNumeros(unittest.TestCase):
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
 
-    
+    @patch(  
+        'builtins.input',
+        return_value='-100'
+    )
+    def test_ingreso_negativo(self, patch_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+   
 
 if __name__ == '__main__':
     unittest.main() 
