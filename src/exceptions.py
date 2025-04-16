c2d44819bceb7a5570807d119796ffaa7e77a6e7
gabiivz <g.vaca@alumno.um.edu.ar>
2025-04-15 21:10:07 -0300

    implementacion de funcion

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,23 +1,14 @@
-#class NumeroDebeSerPositivo(Exception):
-    """Excepción lanzada cuando se ingresa un número negativo."""
-    #pass
+class NumeroDebeSerPositivo(Exception):
 
-#def ingrese_numero():
-    """
-    Solicita al usuario ingresar un número y valida que sea positivo.
+    pass
+
+def ingrese_numero():
     
-    Returns:
-        int: El número ingresado si es válido.
-        
-    Raises:
-        ValueError: Si la entrada no es un número válido.
-        NumeroDebeSerPositivo: Si el número ingresado es negativo.
-    """
- #   entrada = input("Ingrese un número: ")
-  #  try:
-   #     numero = int(entrada)
-    #    if numero < 0:
-     #       raise NumeroDebeSerPositivo("El número debe ser positivo")
-      #  return numero
-    #except ValueError:
-     #   raise ValueError("La entrada debe ser un número válido") 
+    entrada = input("Ingrese un número: ")
+    try:
+        numero = int(entrada)
+        if numero < 0:
+            raise NumeroDebeSerPositivo("El número debe ser positivo")
+        return numero
+    except ValueError:
+        raise ValueError("La entrada debe ser un número válido") 
