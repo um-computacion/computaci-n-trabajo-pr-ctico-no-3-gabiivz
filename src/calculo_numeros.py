2e0af6d585bf2237025bde07c0fe28dd87e83db7
gabiivz <g.vaca@alumno.um.edu.ar>
2025-04-15 21:17:30 -0300

    implementacion de funcion ingrese numero

--- src/calculo_numeros.py
+++ src/calculo_numeros.py
@@ -1,22 +1,3 @@
+def ingrese_numero():
+    pass
 
-#from exceptions import ingrese_numero
-
-#def main():
-    """
-    Programa principal que solicita números al usuario y muestra los resultados.
-    """
-    #while True:
-        #try:
-          #  numero = ingrese_numero()
-         #   print(f"Número válido: {numero}")
-        #except ValueError as e:
-       #     print(f"Error: {e}")
-      #  except NumeroDebeSerPositivo as e:
-     #       print(f"Error: {e}")
-   #     except KeyboardInterrupt:
-    #        print("\nPrograma finalizado.")
-  #          break
-
-#if __name__ == "__main__":
- #   main() 
-    
